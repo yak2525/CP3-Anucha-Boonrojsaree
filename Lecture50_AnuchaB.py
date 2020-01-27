a6fb316e6a3efbe610ec60aacc83a134c83fffaf
yak2525 <yak2525@gmail.com>
2020-01-27 22:49:29 +0700

    Add files via upload

---
+++ Lecture50_AnuchaB.py
@@ -0,0 +1,16 @@
+def add_number(x,y):
+    print(x+y)
+
+def minus_number(x,y):
+    print(x-y)
+
+def mutiply_number(x,y):
+    print(x*y)
+
+def divide_number(x,y):
+    print(x/y)
+
+add_number(1,2)
+minus_number(3,4)
+mutiply_number(5,6)
+divide_number(7,8)
